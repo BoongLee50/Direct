5b25e5585642d20ab426095c0070768618910151
lhl30927 <lhl30927@gmail.com>
2019-11-01 18:20:22 +0900

    [슈팅게임 골격]

--- Direct3DGames/Direct3DGames/GameCode.cpp
+++ Direct3DGames/Direct3DGames/GameCode.cpp
@@ -12,10 +12,22 @@ GameCode::~GameCode(void)
 
 }
 
+BOOL GameCode::CheckCubeIntersection(D3DXVECTOR3* vMin1, D3DXVECTOR3* vMax1, D3DXVECTOR3* vMin2, D3DXVECTOR3* vMax2)
+{
+	if (vMin1->x <= vMax2->x && vMax1->x >= vMin2->x &&
+		vMin1->y <= vMax2->y && vMax1->y >= vMin2->y &&
+		vMin1->z <= vMax2->z && vMax1->z >= vMin2->z)
+		return TRUE;
+
+	return FALSE;
+}
+
 void GameCode::OnInit()
 {
+	int i;
+
 	RECT rect;
-	D3DVIEWPORT9 vp;					//뷰포트, 창의 크기를 정함
+	D3DVIEWPORT9 vp;
 	GetClientRect(m_hWnd, &rect);
 
 	vp.X = 0;
@@ -25,16 +37,16 @@ void GameCode::OnInit()
 	vp.MinZ = 0.0f;
 	vp.MaxZ = 1.0f;
 
-	m_Eye.x = 10.0f;
-	m_Eye.y = 10.0f;
-	m_Eye.z = -15.0f;
+	m_Eye.x = 0.0f;
+	m_Eye.y = 90.0f;
+	m_Eye.z = -5.0f;
 
 	m_At.x = 0.0f;
 	m_At.y = 0.0f;
 	m_At.z = 0.0f;
 
 	m_Up.x = 0.0f;
-	m_Up.y = 1.0f;
+	m_Up.y = 0.1f;
 	m_Up.z = 0.0f;
 
 	D3DXMatrixLookAtLH(&m_matView, &m_Eye, &m_At, &m_Up);
@@ -44,57 +56,402 @@ void GameCode::OnInit()
 	m_pd3dDevice->SetTransform(D3DTS_PROJECTION, &m_matProj);
 	m_pd3dDevice->SetViewport(&vp);
 
-	m_Axis.OnInit(m_pd3dDevice);
+	D3DXCreateTeapot(m_pd3dDevice, &m_pTeapotMesh, NULL);
+	D3DXCreateCylinder(m_pd3dDevice, 2.0f, 2.0f, 10.0f, 15, 10, &m_pPlayerBulletMesh, NULL);
+	D3DXCreateBox(m_pd3dDevice, 1.0f, 1.0f, 1.0f, &m_pEnemyBoxMesh, NULL);
+	D3DXCreateSphere(m_pd3dDevice, 0.2f, 20, 10, &m_pEnemyBulletMesh, NULL);
+	m_Ground.Create(m_pd3dDevice, 20, 6, 2.0f);
 
-	D3DXCreateTeapot( m_pd3dDevice, &m_pTeapotMesh, NULL );
+	//주인공 정보 초기화
+	m_sPlayer.fScale = 0.7f;
+	m_sPlayer.fRotationY = -D3DXToRadian(90);
+	m_sPlayer.vPos = D3DXVECTOR3(0.0f, 0.0f, -9 * 2.0f);
+	m_sPlayer.fVelocity = 0.015f;
+	m_sPlayer.dwBulletFireTime = 150;
+	m_sPlayer.dwOldBulletFireTime = GetTickCount();
+	for (i = 0; i < 10; i++)
+		m_sPlayerBullet[i].nLife = 0;
 
-	m_Ground.Create(m_pd3dDevice, 20, 20, 2.0f);
+	D3DXMatrixScaling(&m_sPlayer.matScale, m_sPlayer.fScale, m_sPlayer.fScale, m_sPlayer.fScale);
+	D3DXMatrixTranslation(&m_sPlayer.matTranslation, m_sPlayer.vPos.x, m_sPlayer.vPos.y, m_sPlayer.vPos.z);
+	D3DXMatrixRotationY(&m_sPlayer.matRotationY, m_sPlayer.fRotationY);
+
+	//주인공 미사일 설정
+	m_sPlayerBulletProperty.fBulletVelcoty = 0.02f;
+	m_sPlayerBulletProperty.fScale = 0.08f;
+	D3DXMatrixScaling(&m_sPlayerBulletProperty.matScale, m_sPlayerBulletProperty.fScale, m_sPlayerBulletProperty.fScale, m_sPlayerBulletProperty.fScale);
+
+	//적 정보 초기화
+	m_EnemyProperty.fScale = 1.0f;
+	m_EnemyProperty.fMoveVelcoty = 0.003f;
+	m_EnemyProperty.dwFireTime = 1200;
+	D3DXMatrixScaling(&m_EnemyProperty.matScale, m_EnemyProperty.fScale, m_EnemyProperty.fScale, m_EnemyProperty.fScale);
+
+	int nRandomNum;
+	for (i = 0; i < 100; i++)
+	{
+		m_Enemy[i].nLife = 0;
+		m_Enemy[i].dwAppearTime = 2000 + (i * 800);
+		m_Enemy[i].dwOldFireTime = GetTickCount();
+		nRandomNum = rand() % 7;
+		if (nRandomNum % 2)
+			m_Enemy[i].vPos.x = -1.0f * nRandomNum;
+		else
+			m_Enemy[i].vPos.x = (float)nRandomNum;
+
+		m_Enemy[i].vPos.y = 0.0f;
+		m_Enemy[i].vPos.z = 20.0f;
+
+		D3DXMatrixTranslation(&m_Enemy[i].matTranslation, m_Enemy[i].vPos.x, m_Enemy[i].vPos.y, m_Enemy[i].vPos.z);
+	}
+
+	//적 미사일 설정
+	m_EnemyBulletProperty.fBulletVelcoty = 0.01f;
+	m_EnemyBulletProperty.fScale = 0.9f;
+	D3DXMatrixScaling(&m_EnemyBulletProperty.matScale, m_EnemyBulletProperty.fScale, m_EnemyBulletProperty.fScale, m_EnemyBulletProperty.fScale);
+
+	for (i = 0; i < 100; i++)
+		m_EnemyBullet[i].nLife = 0;
+
+	m_sPlayer.nLife = 3;
+	m_dwGameStartTime = GetTickCount();
+
+	//주인공 충돌 정점 설정 (최대 최소 정점 값 <= 모델 좌표)
+	D3DXVECTOR3* pVertices;
+	m_pTeapotMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)& pVertices);
+	D3DXComputeBoundingBox(pVertices, m_pTeapotMesh->GetNumVertices(), D3DXGetFVFVertexSize(m_pTeapotMesh->GetFVF()), &m_sPlayer.vMin, &m_sPlayer.vMax);
+	m_pTeapotMesh->UnlockVertexBuffer();
+
+	D3DXVECTOR3 vMin1, vMax1;
+	float fTemp;
+	D3DXVec3TransformCoord(&vMin1, &m_sPlayer.vMin, &m_sPlayer.matRotationY);
+	D3DXVec3TransformCoord(&vMax1, &m_sPlayer.vMax, &m_sPlayer.matRotationY);
+
+	if (vMin1.x > vMax1.x)  //모델좌표에서 회전이 있으므로 최대점과 최소점을 다시 구해야 한다.
+	{
+		fTemp = vMin1.x;
+		vMin1.x = vMax1.x;
+		vMax1.x = fTemp;
+	}
+
+	if (vMin1.y > vMax1.y)
+	{
+		fTemp = vMin1.y;
+		vMin1.y = vMax1.y;
+		vMax1.y = fTemp;
+	}
+
+	if (vMin1.z > vMax1.z)
+	{
+		fTemp = vMin1.z;
+		vMin1.z = vMax1.z;
+		vMax1.z = fTemp;
+	}
+
+	m_sPlayer.vMin = vMin1;
+	m_sPlayer.vMax = vMax1;
+
+	//주인공 미사일 충돌 정점
+	m_pPlayerBulletMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)& pVertices);
+	D3DXComputeBoundingBox(pVertices, m_pPlayerBulletMesh->GetNumVertices(), D3DXGetFVFVertexSize(m_pPlayerBulletMesh->GetFVF()), &m_sPlayerBulletProperty.vMin, &m_sPlayerBulletProperty.vMax);
+	m_pPlayerBulletMesh->UnlockVertexBuffer();
+
+	//적 충돌 정점	
+	m_pEnemyBoxMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)& pVertices);
+	D3DXComputeBoundingBox(pVertices, m_pEnemyBoxMesh->GetNumVertices(), D3DXGetFVFVertexSize(m_pEnemyBoxMesh->GetFVF()), &m_EnemyProperty.vMin, &m_EnemyProperty.vMax);
+	m_pEnemyBoxMesh->UnlockVertexBuffer();
+
+	//적 미사일 충돌 정점
+	m_pEnemyBulletMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)& pVertices);
+	D3DXComputeBoundingBox(pVertices, m_pEnemyBoxMesh->GetNumVertices(), D3DXGetFVFVertexSize(m_pEnemyBulletMesh->GetFVF()), &m_EnemyBulletProperty.vMin, &m_EnemyBulletProperty.vMax);
+	m_pEnemyBulletMesh->UnlockVertexBuffer();
 }
 
 void GameCode::OnRender()
 {
-	D3DXMATRIX matRotationY, matWorld, matTrans, matScaling;
-	float fScaling[3] = { 0.3f, 0.6f, 1.0f };
-	D3DXVECTOR3 vTrans[3] = { D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DXVECTOR3(2.0f, 0.0f, 0.0f), D3DXVECTOR3(5.0f, 0.0f, 0.0f) };
+	int i;
+	D3DXMATRIX matWorld, matScale, matTrans, matRotation;
 
-	D3DXMatrixRotationY(&matRotationY, GetTickCount() * 0.004f);
+	m_Ground.OnRender();
 
-	m_Axis.OnRender();
-
+	// 주인공 미사일
 	m_pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
 	m_pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
 
-	D3DXMatrixRotationY(&matRotationY, GetTickCount() * 0.004f);
+	for (i = 0; i < m_maxBulletCount; i++)
+	{
+		if (m_sPlayerBullet[i].nLife > 0)
+		{
+			matWorld = m_sPlayerBulletProperty.matScale * m_sPlayerBullet[i].matTranslation;
+			m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+			m_pPlayerBulletMesh->DrawSubset(0);
+		}
+	}
 
-	D3DXMatrixScaling(&matScaling, fScaling[0], fScaling[0], fScaling[0]);
-	D3DXMatrixTranslation(&matTrans, vTrans[0].x, vTrans[0].y, vTrans[0].z);
-	matWorld = matScaling * matRotationY * matTrans;		//SRT
-	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+	//적 미사일
+	for (i = 0; i < 100; i++)
+	{
+		if (m_EnemyBullet[i].nLife > 0)
+		{
+			matWorld = m_EnemyBulletProperty.matScale * m_EnemyBullet[i].matTranslation;
+			m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+			m_pEnemyBulletMesh->DrawSubset(0);
+		}
+	}
+
+	//적
+	for (i = 0; i < m_nEnemyIndex; i++)
+	{
+		if (m_Enemy[i].nLife)
+		{
+			matWorld = m_EnemyProperty.matScale * m_Enemy[i].matTranslation;
+			m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+			m_pEnemyBoxMesh->DrawSubset(0);
+		}
+	}
+
+	//주인공 	
+	m_pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+	m_pd3dDevice->SetTransform(D3DTS_WORLD, &m_sPlayer.matWorld);
 	m_pTeapotMesh->DrawSubset(0);
-
-	D3DXMatrixScaling(&matScaling, fScaling[1], fScaling[1], fScaling[1]);
-	D3DXMatrixTranslation(&matTrans, vTrans[1].x, vTrans[1].y, vTrans[1].z);
-	matWorld = matScaling * matRotationY * matTrans;		//SRT
-	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
-	m_pTeapotMesh->DrawSubset(0);
-
-	D3DXMatrixScaling(&matScaling, fScaling[2], fScaling[2], fScaling[2]);
-	D3DXMatrixTranslation(&matTrans, vTrans[2].x, vTrans[2].y, vTrans[2].z);
-	matWorld = matScaling * matTrans * matRotationY;		//STR
-	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
-	m_pTeapotMesh->DrawSubset(0);
-
-	m_Ground.OnRender();
+	m_pd3dDevice->SetRenderState(D3DRS_LIGHTING, TRUE);
 }
 
 void GameCode::OnUpdate()
 {
+	int i, j;
 
+	static DWORD OldTime;
+	DWORD dwCurTime = GetTickCount();
+	m_dwElapsedTime = dwCurTime - OldTime;
+	OldTime = dwCurTime;
+	D3DXMATRIX matWorld;
+
+	// 게임 경과 시간 체크 부분
+	m_dwGameElapsedTime = dwCurTime - m_dwGameStartTime;
+
+	if (GetAsyncKeyState(VK_LEFT) < 0)
+	{
+		if (m_sPlayer.vPos.x - (m_dwElapsedTime * m_sPlayer.fVelocity) >= -6.0f)
+			m_sPlayer.vPos.x -= m_dwElapsedTime * m_sPlayer.fVelocity;
+	}
+
+	if (GetAsyncKeyState(VK_RIGHT) < 0)
+	{
+		if (m_sPlayer.vPos.x + (m_dwElapsedTime * m_sPlayer.fVelocity) <= 6.0f)
+			m_sPlayer.vPos.x += m_dwElapsedTime * m_sPlayer.fVelocity;
+	}
+
+	if (GetAsyncKeyState(VK_UP) < 0)
+	{
+		if (m_sPlayer.vPos.z + (m_dwElapsedTime * m_sPlayer.fVelocity) <= 20.0f)
+			m_sPlayer.vPos.z += m_dwElapsedTime * m_sPlayer.fVelocity;
+	}
+	if (GetAsyncKeyState(VK_DOWN) < 0)
+	{
+		if (m_sPlayer.vPos.z - (m_dwElapsedTime * m_sPlayer.fVelocity) >= -19.0f)
+			m_sPlayer.vPos.z -= m_dwElapsedTime * m_sPlayer.fVelocity;
+	}
+
+	if (GetAsyncKeyState('A') < 0)  // 주인공 미사일 발사
+	{
+		if (dwCurTime - m_sPlayer.dwOldBulletFireTime >= m_sPlayer.dwBulletFireTime)
+		{
+			m_sPlayer.dwOldBulletFireTime = dwCurTime;
+
+			for (i = 0; i < 10; i++)
+			{
+				if (m_sPlayerBullet[i].nLife <= 0)
+				{
+					m_sPlayerBullet[i].nLife = 1;
+					m_sPlayerBullet[i].vPos = m_sPlayer.vPos;
+					m_sPlayerBullet[i].vPos.z += 1.0f;
+					break;
+				}
+			}
+		}
+	}
+
+	//주인공 미사일 이동
+	for (i = 0; i < 10; i++)
+	{
+		if (m_sPlayerBullet[i].nLife > 0)
+		{
+			m_sPlayerBullet[i].vPos.z += m_dwElapsedTime * m_sPlayerBulletProperty.fBulletVelcoty;
+			if (m_sPlayerBullet[i].vPos.z >= 20.0f) // 경계 영역 충돌
+				m_sPlayerBullet[i].nLife = 0;
+			else
+				D3DXMatrixTranslation(&m_sPlayerBullet[i].matTranslation, m_sPlayerBullet[i].vPos.x, m_sPlayerBullet[i].vPos.y, m_sPlayerBullet[i].vPos.z);
+		}
+	}
+
+	//적 출현
+	for (i = m_nEnemyIndex; i < 100; i++)
+	{
+		if (m_Enemy[i].dwAppearTime <= m_dwGameElapsedTime)
+		{
+			m_Enemy[i].nLife = 1;
+			m_nEnemyIndex++;
+		}
+		else {
+			break;
+		}
+	}
+
+	//적 이동과 적 미사일 발사
+	for (i = 0; i < m_nEnemyIndex; i++)
+	{
+		if (m_Enemy[i].nLife)
+		{
+			m_Enemy[i].vPos.z -= m_dwElapsedTime * m_EnemyProperty.fMoveVelcoty;
+			if (m_Enemy[i].vPos.z <= -20.0f) // 경계 영역
+			{
+				m_Enemy[i].nLife = 0;
+				continue;
+			}
+
+			D3DXMatrixTranslation(&m_Enemy[i].matTranslation, m_Enemy[i].vPos.x, m_Enemy[i].vPos.y, m_Enemy[i].vPos.z);
+
+			//살아있는 적만 미사일발사 가능
+			if (dwCurTime - m_Enemy[i].dwOldFireTime >= m_EnemyProperty.dwFireTime)
+			{
+				m_Enemy[i].dwOldFireTime = dwCurTime;
+				for (j = 0; j < 100; j++)
+				{
+					if (!m_EnemyBullet[j].nLife)
+					{
+						m_EnemyBullet[j].nLife = 1;
+						m_EnemyBullet[j].vPos = m_Enemy[i].vPos;
+						m_EnemyBullet[j].vPos.z -= 0.5f; //약간 앞에서 미사일이 발사가 되도록 하기 위해									
+						break; //미사일 하나만 살리는 부분의 끝, 겹치지 않게 
+					}
+				}
+			}
+		}
+	}
+
+	//적 미사일 이동
+	for (i = 0; i < 100; i++)
+	{
+		if (m_EnemyBullet[i].nLife > 0)
+		{
+			m_EnemyBullet[i].vPos.z -= m_dwElapsedTime * m_EnemyBulletProperty.fBulletVelcoty;
+			if (m_EnemyBullet[i].vPos.z <= -20.0f) //경계 영역
+				m_EnemyBullet[i].nLife = 0;
+			else
+				D3DXMatrixTranslation(&m_EnemyBullet[i].matTranslation, m_EnemyBullet[i].vPos.x, m_EnemyBullet[i].vPos.y, m_EnemyBullet[i].vPos.z);
+		}
+	}
+
+	//------------------------------충돌 체크 부분 -----------------------------------------------------------//
+	//주인공과 적 충돌
+	D3DXMatrixTranslation(&m_sPlayer.matTranslation, m_sPlayer.vPos.x, m_sPlayer.vPos.y, m_sPlayer.vPos.z);
+
+	////주인공의 충돌 박스
+	D3DXVECTOR3 vMin1, vMax1, vMin2, vMax2;
+
+	D3DXVec3TransformCoord(&vMin1, &m_sPlayer.vMin, &m_sPlayer.matTranslation);
+	D3DXVec3TransformCoord(&vMax1, &m_sPlayer.vMax, &m_sPlayer.matTranslation);
+
+	for (i = 0; i < m_nEnemyIndex; i++)
+	{
+		if (m_Enemy[i].nLife > 0)
+		{
+			D3DXMatrixTranslation(&m_Enemy[i].matTranslation, m_Enemy[i].vPos.x, m_Enemy[i].vPos.y, m_Enemy[i].vPos.z);
+			matWorld = m_EnemyProperty.matScale * m_Enemy[i].matTranslation;
+
+			D3DXVec3TransformCoord(&vMin2, &m_EnemyProperty.vMin, &matWorld);
+			D3DXVec3TransformCoord(&vMax2, &m_EnemyProperty.vMax, &matWorld);
+
+			if (CheckCubeIntersection(&vMin1, &vMax1, &vMin2, &vMax2))
+			{
+				m_Enemy[i].nLife = 0;
+				m_sPlayer.nLife--;
+			}
+		}
+	}
+
+	//주인공과 적 미사일 충돌
+	for (i = 0; i < 100; i++)
+	{
+		if (m_EnemyBullet[i].nLife > 0)
+		{
+			D3DXMatrixTranslation(&m_EnemyBullet[i].matTranslation, m_EnemyBullet[i].vPos.x, m_EnemyBullet[i].vPos.y, m_EnemyBullet[i].vPos.z);
+			matWorld = m_EnemyBullet[i].matTranslation;
+			D3DXVec3TransformCoord(&vMin2, &m_EnemyBulletProperty.vMin, &matWorld);
+			D3DXVec3TransformCoord(&vMax2, &m_EnemyBulletProperty.vMax, &matWorld);
+
+			if (CheckCubeIntersection(&vMin1, &vMax1, &vMin2, &vMax2))
+			{
+				m_EnemyBullet[i].nLife = 0;
+				m_sPlayer.nLife--;
+			}
+		}
+	}
+
+	//주인공 미사일과 적 충돌
+	for (i = 0; i < 10; i++)
+	{
+		if (m_sPlayerBullet[i].nLife > 0)
+		{
+			D3DXMatrixTranslation(&m_sPlayerBullet[i].matTranslation, m_sPlayerBullet[i].vPos.x, m_sPlayerBullet[i].vPos.y, m_sPlayerBullet[i].vPos.z);
+			matWorld = m_sPlayerBulletProperty.matScale * m_sPlayerBullet[i].matTranslation;
+			D3DXVec3TransformCoord(&vMin1, &m_sPlayerBulletProperty.vMin, &matWorld);
+			D3DXVec3TransformCoord(&vMax1, &m_sPlayerBulletProperty.vMax, &matWorld);
+
+			//적과 충돌 체크
+			for (j = 0; j < m_nEnemyIndex; j++)
+			{
+				if (m_Enemy[j].nLife > 0)
+				{
+					D3DXMatrixTranslation(&m_Enemy[j].matTranslation, m_Enemy[j].vPos.x, m_Enemy[j].vPos.y, m_Enemy[j].vPos.z);
+					matWorld = m_EnemyProperty.matScale * m_Enemy[j].matTranslation;
+
+					D3DXVec3TransformCoord(&vMin2, &m_EnemyProperty.vMin, &matWorld);
+					D3DXVec3TransformCoord(&vMax2, &m_EnemyProperty.vMax, &matWorld);
+
+					if (CheckCubeIntersection(&vMin1, &vMax1, &vMin2, &vMax2))
+					{
+						m_Enemy[j].nLife = 0;
+						m_sPlayerBullet[i].nLife = 0;
+						break;
+					}
+				}
+			}
+
+			//주인공 미사일과 적 미사일 충돌 체크
+			if (m_sPlayerBullet[i].nLife > 0)
+			{
+				for (j = 0; j < 100; j++)
+				{
+					if (m_EnemyBullet[j].nLife > 0)
+					{
+						D3DXMatrixTranslation(&m_EnemyBullet[j].matTranslation, m_EnemyBullet[j].vPos.x, m_EnemyBullet[j].vPos.y, m_EnemyBullet[j].vPos.z);
+						matWorld = m_EnemyBullet[j].matTranslation;
+						D3DXVec3TransformCoord(&vMin2, &m_EnemyBulletProperty.vMin, &matWorld);
+						D3DXVec3TransformCoord(&vMax2, &m_EnemyBulletProperty.vMax, &matWorld);
+
+						if (CheckCubeIntersection(&vMin1, &vMax1, &vMin2, &vMax2))
+						{
+							m_EnemyBullet[j].nLife = 0;
+							m_sPlayerBullet[i].nLife = 0;
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	D3DXMatrixTranslation(&m_sPlayer.matTranslation, m_sPlayer.vPos.x, m_sPlayer.vPos.y, m_sPlayer.vPos.z);
+	m_sPlayer.matWorld = m_sPlayer.matScale * m_sPlayer.matRotationY * m_sPlayer.matTranslation;
 }
 
 void GameCode::OnRelease()
 {
+	m_pEnemyBoxMesh->Release();
+	m_pEnemyBulletMesh->Release();
 	m_pTeapotMesh->Release();
-	m_Axis.OnRelease();
+	m_pPlayerBulletMesh->Release();
 	m_Ground.OnRelease();
 }
--- Direct3DGames/Direct3DGames/GameCode.h
+++ Direct3DGames/Direct3DGames/GameCode.h
@@ -6,32 +6,102 @@
 #include "Axis.h"
 #include "Ground.h"
 
-struct TEAPOT_PROPERTY
+struct BULLET					//총알 하나하나의 속성
 {
-	float fScaling;
-	float fRotX, fRotY, fRotZ;
-	D3DXVECTOR3 vTranslation;
-	TEAPOT_PROPERTY() : fScaling(1.0f) {}
+	int nLife;
+	D3DXVECTOR3 vPos;
+	D3DXMATRIX matTranslation;
+};
+
+struct BULLET_PROPERTY			//총알 전체속성
+{
+	float fBulletVelcoty;
+	float fScale;
+	D3DXMATRIX matScale;
+
+	D3DXVECTOR3 vMin, vMax;		//충돌 좌표
+};
+
+struct PLAYER
+{
+	int nLife;
+	float fScale;
+	float fRotationY;
+	D3DXVECTOR3 vPos;
+	float fVelocity;
+
+	DWORD dwBulletFireTime;			//미사일 발사 간격
+	DWORD dwOldBulletFireTime;		//이전 미사일 발사 시간
+
+	D3DXMATRIX matWorld;			//월드 행렬
+	D3DXMATRIX matScale;			//크기 행렬
+	D3DXMATRIX matRotationY;		//회전 행렬
+	D3DXMATRIX matTranslation;		//이동 행렬
+
+	D3DXVECTOR3 vMin, vMax;		//충돌 좌표
+};
+
+struct ENEMY				//적의 전체속성
+{
+	int nLife;
+	DWORD dwAppearTime;		//출현시간
+	D3DXVECTOR3 vPos;
+
+	DWORD dwOldFireTime;	//미사일 발사 이전 시간
+	D3DXMATRIX matTranslation;
+};
+
+struct ENEMY_PROPERTY		//적의 개별속성
+{
+	float fMoveVelcoty;
+	float fScale;
+
+	DWORD dwFireTime;		//미사일 발사 시간 간격
+	D3DXMATRIX matScale;
+
+	D3DXVECTOR3 vMin, vMax;		//충돌 좌표
 };
 
 class GameCode : public CD3DApp
 {
-	CGround m_Ground;
-
-	D3DXMATRIX m_matView;
-	D3DXMATRIX m_matProj;
-	D3DXVECTOR3 m_Eye, m_At, m_Up;
-	CAxis m_Axis;
-
-	TEAPOT_PROPERTY m_TeapotProperty[3];
-	LPD3DXMESH m_pTeapotMesh;
-	int m_nAngle;
-
 	virtual void OnInit();
 	virtual void OnRender();
 	virtual void OnUpdate();
 	virtual void OnRelease();
 
+	static const int m_maxBulletCount = 15;
+
+	D3DXMATRIX m_matView;
+	D3DXMATRIX m_matProj;
+	D3DXVECTOR3 m_Eye, m_At, m_Up;
+
+	CGround m_Ground;
+
+	LPD3DXMESH m_pTeapotMesh;
+	LPD3DXMESH m_pPlayerBulletMesh;
+	LPD3DXMESH m_pEnemyBoxMesh;
+	LPD3DXMESH m_pEnemyBulletMesh;
+
+	PLAYER m_sPlayer;
+	BULLET_PROPERTY m_sPlayerBulletProperty;
+	BULLET m_sPlayerBullet[m_maxBulletCount];
+
+	// 적 캐릭터 
+	ENEMY_PROPERTY m_EnemyProperty;
+	ENEMY m_Enemy[100];				//적 캐릭터 100개 
+	BULLET_PROPERTY m_EnemyBulletProperty;
+	BULLET m_EnemyBullet[100];
+	int m_nEnemyIndex;				//적 캐릭터 구분할 수 있도록 하는 배열의 인덱스 값
+
+	DWORD m_dwElapsedTime;
+
+	// 스테이지 정보
+	DWORD m_dwGameStartTime;
+	DWORD m_dwGameElapsedTime;
+
+	int m_nEnemyCount;
+
+	BOOL CheckCubeIntersection(D3DXVECTOR3* vMin1, D3DXVECTOR3* vMax1, D3DXVECTOR3* vMin2, D3DXVECTOR3* vMax2);
 public:
 	GameCode(void);
 	~GameCode(void);
